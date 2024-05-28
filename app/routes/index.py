51c448c9230a229f7aa64d1a837cfe691d54c9c7
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2024-05-28 21:40:10 +0500

    Add info alert about an existing connection

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,13 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 
 from . import env
+from ..dependencies import connections
 
 router = APIRouter()
 
 
 @router.get("/")
-def index(alert: str = ""):
+def index(alert_type: str = "warning", alert: str = "", request: Request = Request):
     template = env.get_template("index.html")
-    page = template.render(alert=alert)
+    user_agent = request.headers.get("user-agent")
+    if not alert and connections.get(user_agent) and connections[user_agent].get_transport().active:
+        alert = "У вас уже есть текущее соединение"
+        alert_type = "info"
+    page = template.render(alert_type=alert_type, alert=alert)
     return HTMLResponse(page)
